19046c74b9cfcb152a1ae43c55c90a92f5ca200f
mhmmdjafarg <mjafarashadiq712@gmail.com>
2024-08-11 23:58:14 +0700

    feat: add padding to the grid

--- src/main/java/org/learn/ui/FileIO.java
+++ src/main/java/org/learn/ui/FileIO.java
@@ -9,6 +9,8 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class FileIO {
+    private static final int PADDING = 1;
+
     public static Grid loadGrid(String filename) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(filename));
 
@@ -23,15 +25,18 @@ public class FileIO {
         }
         reader.close();
 
-        Grid grid = new Grid(height, width);
+        // Calculate new dimensions with padding
+        int paddedWidth = width + 2 * PADDING;
+        int paddedHeight = height + 2 * PADDING;
+        Grid grid = new Grid(paddedHeight, paddedWidth);
 
         // Read the file again to populate the grid
         reader = new BufferedReader(new FileReader(filename));
-        int row = 0;
+        int row = PADDING;
         while ((line = reader.readLine()) != null) {
             for (int col = 0; col < line.length(); col++) {
                 CellState state = line.charAt(col) == '#' ? CellState.ALIVE : CellState.EMPTY;
-                grid.setCell(row, col, new Cell(state));
+                grid.setCell(row, col+PADDING, new Cell(state));
             }
             row++;
         }
--- src/main/java/org/learn/util/GridInitializer.java
+++ src/main/java/org/learn/util/GridInitializer.java
@@ -8,18 +8,20 @@ import java.util.Random;
 
 public class GridInitializer {
     private static final int GRID_SIZE = 10;
+    private static final int PADDING = 1;
 
     public static Grid generateRandomGrid() {
-        Cell[][] cells = new Cell[GRID_SIZE][GRID_SIZE];
+        Grid grid = new Grid(GRID_SIZE + 2 * PADDING, GRID_SIZE + 2 * PADDING);
         Random random = new Random();
 
-        for (int i = 0; i < GRID_SIZE; i++) {
+        for (int i = PADDING; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
                 // Randomly set the cell to be ALIVE or DEAD
                 boolean isAlive = random.nextBoolean();
-                cells[i][j] = new Cell(isAlive ? CellState.ALIVE : CellState.EMPTY);
+                Cell cell = new Cell(isAlive ? CellState.ALIVE : CellState.EMPTY);
+                grid.setCell(i, j + PADDING, cell);
             }
         }
-        return new Grid(cells);
+        return grid;
     }
 }
